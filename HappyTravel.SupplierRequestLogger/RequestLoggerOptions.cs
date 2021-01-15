dcd979cae24afc6fa5c59d80cb9fa097bd012bc1
Denis Nikolaev <dfs.str@gmail.com>
2021-01-15 18:33:58 +0500

    fixed System.Reflection.TargetInvocationException

--- HappyTravel.SupplierRequestLogger/RequestLoggerOptions.cs
+++ HappyTravel.SupplierRequestLogger/RequestLoggerOptions.cs
@@ -6,7 +6,6 @@ namespace HappyTravel.SupplierRequestLogger
 {
     public class RequestLoggerOptions
     {
-        [Required]
         public string Endpoint { get; set; } = string.Empty;
         public Func<HttpRequestMessage, bool>? LoggingCondition { get; set; }
     }
